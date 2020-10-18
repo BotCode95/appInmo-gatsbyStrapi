f65bc7c03927cc5d0c25272610ecaad931e7b17b
BotCode95 <patribottino95@gmail.com>
2020-10-18 01:13:20 -0300

    card creadas

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,7 @@ module.exports = {
       options:{
         apiURL: 'http://localhost:1337',
         queryLimit: 1000,
-        contentType: ['propiedades', 'paginas', 'categorias']
+        contentTypes: ['propiedades', 'paginas', 'categorias']
       }
     }
     // {
